ce56ce6e396444da2972b7ac94d869b5a0472314
Tristan <tristanw747@gmail.com>
2023-04-21 18:02:17 -0700

    page format using Outlet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider, } from "react-router-dom";
 import Home from './pages/Home/Home'
 import Product from './pages/Product/Product';
 import Products from './pages/Products/Products';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+
+
+function Layout() {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 
 const router = createBrowserRouter([
   {
-    path:"/", 
-    element:<Home/>
-  },
-  {
-    path:"/products/:id", 
-    element:<Products/>
-  },
-  {
-    path:"/product/:id", 
-    element:<Product/>
-  },
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/products/:id",
+        element: <Products />
+      },
+      {
+        path: "/product/:id",
+        element: <Product />
+      }
+    ]
+  }
 ])
 
 
