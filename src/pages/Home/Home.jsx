beba468ae6b5447ebd5472e691aa902159a34bd7
Tristan <tristanw747@gmail.com>
2023-04-23 18:31:46 -0700

    Homepage finished

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Slider from '../../components/Slider/Slider'
 import "./Home.scss"
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import Categories from '../../components/Categories/Categories'
+import Contact from '../../components/Contact/Contact'
 function Home() {
   return (
     <div className='home'>
@@ -10,6 +11,7 @@ function Home() {
       <FeaturedProducts type="featured"/>
       <Categories/>
       <FeaturedProducts type="trending"/>
+      <Contact/>
     </div>
   )
 }
